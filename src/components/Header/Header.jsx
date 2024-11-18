17e7b6ae5bf6303977ef12441c541bf70cbe56d6
Lucas Seccatto <lucasseccatto1@gmail.com>
2024-11-18 15:44:04 -0300

    atualiza header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+import Inicio from "../../pages/inicio/Inicio";
+import LivrosDoados from "../../pages/livrosDoados/LivrosDoados";
+import QueroDoar from "../../pages/queroDoar/QueroDoar";
+
 import Logo from "../../assets/logo-livro.png";
 import Lupa from "../../assets/lupa-header.png";
+
+import s from "./header.module.scss";
 
 export default function Header() {
   return (
     <BrowserRouter>
       <header>
-        <section>
+        <section className={s.logoHeader}>
           <img
             src={Logo}
             alt="Imagem de uma ilustração de um livro aberto, com páginas brancas e capa azul."
@@ -15,37 +21,44 @@ export default function Header() {
           <h1>Livros Vai na Web</h1>
         </section>
 
-        <nav>
+        <nav className={s.navHeader}>
           <ul>
             <li>
-              <Link>Início</Link>
+              <Link className={s.link} to="/">
+                Início
+              </Link>
             </li>
             <li>
-              <Link>Livros Doados</Link>
+              <Link className={s.link} to="livros-doados">
+                Livros Doados
+              </Link>
             </li>
             <li>
-              <Link>Quero Doar</Link>
+              <Link className={s.link} to="quero-doar">
+                Quero Doar
+              </Link>
             </li>
           </ul>
         </nav>
 
-        <section>
+        <section className={s.barraDeBusca}>
           <input
             type="search"
             name=""
             id=""
             placeholder="O que você procura?"
           />
+
+          <button>
+            <img src={Lupa} alt="Imagem de uma lupa branca." />
+          </button>
         </section>
-        <button>
-          <img src={Lupa} alt="Imagem de uma lupa branca." />
-        </button>
       </header>
 
       <Routes>
-        <Route />
-        <Route />
-        <Route />
+        <Route path="/" element={<Inicio />} />
+        <Route path="/livros-doados" element={<LivrosDoados />} />
+        <Route path="/quero-doar" element={<QueroDoar />} />
       </Routes>
     </BrowserRouter>
   );
