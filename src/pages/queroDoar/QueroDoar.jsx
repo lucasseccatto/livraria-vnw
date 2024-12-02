bbb312ed07700ba8d259c675569615c552a1eb28
Lucas Seccatto <lucasseccatto1@gmail.com>
2024-12-02 15:30:43 -0300

    refatoracao da pagina queroDoar

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,3 +1,30 @@
+import s from "./queroDoar.module.scss";
+
+import iconeLivro from "../../assets/icone-livro.png";
+
 export default function QueroDoar() {
-  return <h1>EU SOU O QUERO DOAR</h1>;
+  return (
+    <section className={s.queroDoarSection}>
+      <p>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro
+      </p>
+
+      <form action="">
+        <div>
+          <img
+            src={iconeLivro}
+            alt="Imagem com icone de livro aberto com borda azul."
+          />
+          <h2>Informações do Livro</h2>
+        </div>
+
+        <input type="text" name="" id="" placeholder="Título" />
+        <input type="text" name="" id="" placeholder="Categoria" />
+        <input type="text" name="" id="" placeholder="Autor" />
+        <input type="text" name="" id="" placeholder="Link da Imagem" />
+        <input className={s.buttonDoar} type="submit" value="Doar" />
+      </form>
+    </section>
+  );
 }
