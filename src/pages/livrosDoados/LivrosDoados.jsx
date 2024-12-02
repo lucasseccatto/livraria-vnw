f8851751ee735df1de0c666a80386d590140ce12
Lucas Seccatto <lucasseccatto1@gmail.com>
2024-12-02 15:30:28 -0300

    refatoracao da pagina livrosDoados

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,3 +1,24 @@
+import livroProtagonista from "../../assets/livro-protagonista.png";
+import s from "./livrosDoados.module.scss";
+
 export default function LivrosDoados() {
-  return <h1>EU SOU O LIVROS DOADOS</h1>;
+  return (
+    <section className={s.livrosDoadosSection}>
+      <h2>Livros Doados</h2>
+
+      <sectionc className={s.containerCards}>
+        <section>
+          <img
+            src={livroProtagonista}
+            alt="Imagem do livro 'O Protagonista'."
+          />
+          <div>
+            <h3>O Protagonista</h3>
+            <p>Susanne Andrade</p>
+            <p>Ficção</p>
+          </div>
+        </section>
+      </sectionc>
+    </section>
+  );
 }
