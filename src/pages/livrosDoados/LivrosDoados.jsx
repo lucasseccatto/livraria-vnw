72551cc2fdffc69dd192b345d87523af881fbb51
Lucas Seccatto <lucasseccatto1@gmail.comm>
2025-04-02 15:14:12 -0300

    adiciona consumo da api em livros doados

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,24 +1,36 @@
-import livroProtagonista from "../../assets/livro-protagonista.png";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import s from "./livrosDoados.module.scss";
 
 export default function LivrosDoados() {
+  const [livros, setLivros] = useState([]);
+
+  const getLivros = async () => {
+    const resposta = await axios.get(
+      "https://api-vnw-doacao-de-livros.onrender.com/livros"
+    );
+    setLivros(resposta.data);
+  };
+
+  useEffect(() => {
+    getLivros();
+  }, []);
+
   return (
     <section className={s.livrosDoadosSection}>
       <h2>Livros Doados</h2>
 
-      <sectionc className={s.containerCards}>
-        <section>
-          <img
-            src={livroProtagonista}
-            alt="Imagem do livro 'O Protagonista'."
-          />
-          <div>
-            <h3>O Protagonista</h3>
-            <p>Susanne Andrade</p>
-            <p>Ficção</p>
-          </div>
-        </section>
-      </sectionc>
+      <section className={s.containerCards}>
+        {livros.map((item) => (
+          <section key={item.id}>
+            <img src={item.image_url} alt={`Titulo do livro ${item.titulo}`} />
+            <h3>{item.titulo}</h3>
+            <p>{item.autor}</p>
+            <p>{item.categoria}</p>
+          </section>
+        ))}
+      </section>
     </section>
   );
 }
